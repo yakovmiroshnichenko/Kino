c516f54840b6db849bb7fcb1b0f55d7cbadfabfe
yakov <yakov.miroshnichenko@student.sumdu.edu.ua>
2023-08-22 18:47:46 +0300

    MakeFIle + .env

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 run:
 	python manage.py runserver
+migrate:
+	python manage.py migrate
+migration:
+	python manage.py makemigrations
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+ENV_PATH = '.env'
+load_dotenv(dotenv_path=ENV_PATH)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2g8b#gjjci&f!v6=pr%=g^()8-jhpr4-dz$#cvy=reirscrkw$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
